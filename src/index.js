2e2b5ecb92a55007774371540ef6db320973476d
Kavyashri LH <kavyashrilh@gmail.com>
2026-05-27 20:54:09 +0530

    additional

--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './pages/Home/HomePage';
+
+import PGDetails from './pages/PG/PGDetails';
+import AddListing from './pages/AddListing.js';
+import './index.js'
+import Login from './pages/LoginPage/Login.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar />
+    <Routes>
+    <Route path='/' element={<HomePage />} />
+      {/* <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} /> */}
+      <Route path="/pg/:id" element={<PGDetails />} />
+      <Route path="/addListing" element={<AddListing />} />
+    </Routes>
+  
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
